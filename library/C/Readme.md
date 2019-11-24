119f4e76d3b43ec08510147199de9628df2af0f7
jvelilla <javier.hector@gmail.com>
2019-11-24 12:47:43 -0300

    Updated TensorFlow API Added Windows scripts to automate the wrapping process.

---
+++ library/C/Readme.md
@@ -0,0 +1,7 @@
+The folders `include` and `lib` come from Tensorflow C setup for windows using the following link
+https://storage.googleapis.com/tensorflow/libtensorflow/libtensorflow-cpu-windows-x86_64-1.14.0.zip 
+
+The zip file does not contain a required header file `tf_attrtype.h`.
+
+
+
--- library/post_script.bat
+++ library/post_script.bat
@@ -1,11 +1,5 @@
-@echo off
+@echo on
 title post_process script
 
-rem copy c file is there any
-copy .\manual_wrapper\c\src\*.c  .\generated_wrapper\c\src	
-
-rem copy geant script
-copy finish_freezing.eant  .\generated_wrapper\c\src\geant.eant	
-
-rem copy Makefile script
-copy Makefile-win.SH  .\generated_wrapper\c\src
+cd generated_wrapper/c/src/
+finish_freezing -library
